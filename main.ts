1b9d259b7b920cce47ee101405ffda03e06ffc94
Just-An-Average-Toaster <114497348+Just-An-Average-Toaster@users.noreply.github.com>
2022-10-05 09:22:09 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -150,3 +150,24 @@ mySprite2 = sprites.create(img`
 scene.cameraFollowSprite(mySprite2)
 mySprite2.ay = 500
 controller.moveSprite(mySprite2, 100, 0)
+let myEnemy = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+forever(function () {
+    tiles.placeOnRandomTile(myEnemy, assets.tile`roach`)
+})
