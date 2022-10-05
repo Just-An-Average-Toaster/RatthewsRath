62d26c24354f564bf82c18e53d87933a75d82d25
Spirul <71271833+Spirul@users.noreply.github.com>
2022-10-05 09:34:01 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,22 @@
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite2.vy += -200
-    pause(825)
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Sewage`, function (sprite, location) {
+	
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`EndPipe`, function (sprite, location) {
+    game.over(true, effects.splatter)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
+    if (mySprite2.vy == 0) {
+        mySprite2.vy += -200
+    }
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite2.setImage(assets.image`RatthewOther`)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite2.setImage(assets.image`Ratthew`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`Spikes`, function (sprite, location) {
+    game.over(false, effects.slash)
 })
 let mySprite2: Sprite = null
 tiles.setCurrentTilemap(tilemap`level1`)
@@ -129,24 +142,9 @@ scene.setBackgroundImage(img`
     dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
     `)
-mySprite2 = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
+mySprite2 = sprites.create(assets.image`Ratthew`, SpriteKind.Player)
 scene.cameraFollowSprite(mySprite2)
 mySprite2.ay = 500
+game.over(false, effects.melt)
 controller.moveSprite(mySprite2, 100, 0)
+mySprite2.setPosition(45, 265)
